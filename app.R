00096289cfae278b7e72a6fcd3266cfdb4e3c1c3
Subi <subirnair@gmail.com>
2021-12-06 04:04:23 -0700

    heatmap + timeout

--- app.R
+++ app.R
@@ -10,27 +10,27 @@ library(circlize) # Used for colorRamp2 function
 library(randomcoloR)
 
 # Script for timeout feature
-# timeoutSeconds <- 60*15
-# inactivity <- sprintf("function idleTimer() {
-# var t = setTimeout(logout, %s);
-# window.onmousemove = resetTimer; // catches mouse movements
-# window.onmousedown = resetTimer; // catches mouse movements
-# window.onclick = resetTimer;     // catches mouse clicks
-# window.onscroll = resetTimer;    // catches scrolling
-# window.onkeypress = resetTimer;  //catches keyboard actions
-# function logout() {
-# Shiny.setInputValue('timeOut', '%ss')
-# }
-# function resetTimer() {
-# clearTimeout(t);
-# t = setTimeout(logout, %s);  // time is in milliseconds (1000 is 1 second)
-# }
-# }
-# idleTimer();", timeoutSeconds*1000, timeoutSeconds, timeoutSeconds*1000)
+timeoutSeconds <- 60*15
+inactivity <- sprintf("function idleTimer() {
+var t = setTimeout(logout, %s);
+window.onmousemove = resetTimer; // catches mouse movements
+window.onmousedown = resetTimer; // catches mouse movements
+window.onclick = resetTimer;     // catches mouse clicks
+window.onscroll = resetTimer;    // catches scrolling
+window.onkeypress = resetTimer;  //catches keyboard actions
+function logout() {
+Shiny.setInputValue('timeOut', '%ss')
+}
+function resetTimer() {
+clearTimeout(t);
+t = setTimeout(logout, %s);  // time is in milliseconds (1000 is 1 second)
+}
+}
+idleTimer();", timeoutSeconds*1000, timeoutSeconds, timeoutSeconds*1000)
 
 ui <- fluidPage(
   
-  #shinyUI(fluidPage(tags$script(inactivity))),
+  shinyUI(fluidPage(tags$script(inactivity))),
   
   tabsetPanel(  
     tabPanel("Processor", fluid = TRUE,
@@ -605,16 +605,16 @@ server <- function(input, output, session) {
     return(mgk_table)
   })
   
-  # observeEvent(input$timeOut, { 
-  #   # Modified from: https://stackoverflow.com/questions/33839543/shiny-server-session-time-out-doesnt-work
-  #   showModal(modalDialog(
-  #     title = "Inactivity Timeout",
-  #     paste("Session timeout due to", input$timeOut, "inactivity -", Sys.time()),
-  #     footer = NULL
-  #   ))
-  #   stopApp() # I made this change so that the app closes instead of the window only
-  # })
-  # 
+  observeEvent(input$timeOut, {
+    # Modified from: https://stackoverflow.com/questions/33839543/shiny-server-session-time-out-doesnt-work
+    showModal(modalDialog(
+      title = "Inactivity Timeout",
+      paste("Session timeout due to", input$timeOut, "inactivity -", Sys.time()),
+      footer = NULL
+    ))
+    stopApp() # I made this change so that the app closes instead of the window only
+  })
+
 
   
 }
